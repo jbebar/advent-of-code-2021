c261eac30825b7333f31409b86c06a438a074f26
jbebar <jubarbe31@gmail.com>
2021-12-06 23:22:36 +0100

    wip: day 5B

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -10,55 +10,6 @@ class Day5A : Day() {
         }.filter { (start, end) ->
             start.x == end.x || start.y == end.y
         }.flatMap { (start, end) ->
-            toHorizontalOrVerticalSegment(start, end)
-        }.fold(mutableMapOf<Point, Int>()) { acc, point ->
-            acc.apply {
-                if (acc[point] != null) {
-                    acc[point] = acc[point]!! + 1
-                } else {
-                    acc[point] = 1
-                }
-            }
-        }.count {
-            it.value >= 2
-        }
-    }
-
-    private fun toHorizontalOrVerticalSegment(startPoint: Point, endPoint: Point) =
-        if (startPoint.x == endPoint.x) {
-            if (startPoint.y > endPoint.y) {
-                (endPoint.y..startPoint.y).map { y ->
-                    Point(y, startPoint.x)
-                }
-            } else {
-                (startPoint.y..endPoint.y).map { y ->
-                    Point(y, startPoint.x)
-                }
-            }
-        } else {
-            if (startPoint.x > endPoint.x) {
-                (endPoint.x..startPoint.x).map { x ->
-                    Point(startPoint.y, x)
-                }
-            } else {
-                (startPoint.x..endPoint.x).map { x ->
-                    Point(startPoint.y, x)
-                }
-            }
-        }
-
-
-}
-
-object Day5B : Day() {
-
-    override fun run(): Int {
-        val inputLines = readLines()
-        return inputLines.map {
-            it.first().toPoint() to it.last().toPoint()
-        }.filter { (start, end) ->
-            start.x == end.x || start.y == end.y
-        }.flatMap { (start, end) ->
             toSegment(start, end)
         }.fold(mutableMapOf<Point, Int>()) { acc, point ->
             acc.apply {
@@ -73,31 +24,56 @@ object Day5B : Day() {
         }
     }
 
-    private fun toSegment(startPoint: Point, endPoint: Point): List<Point> {
-        return if (startPoint.x == endPoint.x) {
-            if (startPoint.y > endPoint.y) {
-                (endPoint.y..startPoint.y).map { y ->
-                    Point(y, startPoint.x)
-                }
-            } else {
-                (startPoint.y..endPoint.y).map { y ->
-                    Point(y, startPoint.x)
-                }
-            }
-        } else {
-            if (startPoint.x > endPoint.x) {
-                (endPoint.x..startPoint.x).map { x ->
-                    Point(startPoint.y, x)
-                }
-            } else {
-                (startPoint.x..endPoint.x).map { x ->
-                    Point(startPoint.y, x)
+}
+
+
+class Day5B : Day() {
+
+    override fun run(): Int {
+        val inputLines = readLines()
+        return inputLines.map {
+            it.first().toPoint() to it.last().toPoint()
+        }.map { (start, end) ->
+            toSegment(start, end)
+        }.flatten().fold(mutableMapOf<Point, Int>()) { acc, point ->
+            acc.apply {
+                if (acc.contains(point)) {
+                    acc[point] = acc[point]!! + 1
+                } else {
+                    acc[point] = 1
                 }
             }
+        }.count {
+            it.value >= 2
         }
     }
 
 }
+
+fun toSegment(startPoint: Point, endPoint: Point): List<Point> {
+    val horizontalPart = toCoordinateSuite(startPoint.y, endPoint.y)
+    val verticalPart = toCoordinateSuite(startPoint.x, endPoint.x)
+    return if (horizontalPart.size == verticalPart.size) {
+        horizontalPart.zip(verticalPart).map { (y, x) ->
+            Point(y, x)
+        }
+    } else if (verticalPart.isEmpty()) {
+        horizontalPart.map { Point(y = it, x = startPoint.x) }
+    } else if (horizontalPart.isEmpty()) {
+        verticalPart.map { Point(y = startPoint.y, x = it) }
+    } else {
+        emptyList()
+    }
+}
+
+private fun toCoordinateSuite(start: Int, end: Int) =
+    if (start < end) {
+        (start..end).toList()
+    } else if (start == end) {
+        emptyList()
+    } else {
+        (end..start).toList()
+    }
 
 private fun String.toPoint() = split(",").let { coordinates -> Point(y = coordinates[0].toInt(), x = coordinates[1].toInt()) }
 
---
+++ src/test/kotlin/Day5KtTest.kt
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test
+
+internal class Day5KtTest {
+
+    @Test
+    internal fun `should return segment for vertical line`() {
+        val segmentPoints = toSegment(Point(0, 0), Point(0, 2))
+        assert(segmentPoints.containsAll(listOf(Point(0, 0), Point(0, 1), Point(0, 2))))
+    }
+
+    @Test
+    internal fun `should return segment for horizontal line`() {
+        val segmentPoints = toSegment(Point(0, 0), Point(2, 0))
+        assert(segmentPoints.containsAll(listOf(Point(2, 0), Point(1, 0), Point(0, 0))))
+    }
+
+    @Test
+    internal fun `should return segment for diagonal line at 45 degrees`() {
+        val segmentPoints = toSegment(Point(0, 0), Point(2, 2))
+        assert(segmentPoints.containsAll(listOf(Point(0, 0), Point(1, 1), Point(2, 2))))
+    }
+
+
+    @Test
+    internal fun `should return segment for diagonal line at 45 degrees reverted`() {
+        val segmentPoints = toSegment(Point(2, 2), Point(0, 0))
+        assert(segmentPoints.containsAll(listOf(Point(0, 0), Point(1, 1), Point(2, 2))))
+    }
+
+    @Test
+    internal fun `should return segment for diagonal line at 45 degrees not from origin`() {
+        val segmentPoints = toSegment(Point(2, 3), Point(5, 6))
+        assert(segmentPoints.containsAll(listOf(Point(2, 3), Point(3, 4), Point(4, 5), Point(5, 6))))
+    }
+
+
+}
