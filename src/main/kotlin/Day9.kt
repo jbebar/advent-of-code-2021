51536775c100903462e5ad1ea60953ef7577e187
jbebar <jubarbe31@gmail.com>
2021-12-15 23:36:39 +0100

    clean

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,9 +1,5 @@
 @file:Suppress("unused")
 
-fun findLowPoints(heatPoints: Map<Pair<Int, Int>, Int>) {
-
-}
-
 class Day9A : Day() {
 
     override fun run(): Long {
@@ -11,26 +7,6 @@ class Day9A : Day() {
     }
 
 }
-
-fun String.countRiskLevel(): Int {
-    val map = this.read()
-    return map.entries.map {
-        val y = it.key.first
-        val x = it.key.second
-        val value = map[(y to x)]
-        checkNotNull(value)
-        val isDiagonalLower = map[(y + 1 to x + 1)] strictlyAbove value && map[(y - 1 to x - 1)] strictlyAbove value
-        val isVerticalLower = map[(y + 1 to x)] strictlyAbove value && map[(y - 1 to x)] strictlyAbove value
-        val isHorizontalLower = map[(y to x + 1)] strictlyAbove value && map[(y to x - 1)] strictlyAbove value
-        if (isDiagonalLower && isVerticalLower && isHorizontalLower) {
-            value + 1
-        } else {
-            0
-        }
-    }.sum()
-}
-
-private infix fun Int?.strictlyAbove(other: Int) = this == null || this > other
 
 fun String.read(): Map<Pair<Int, Int>, Int> {
     val heatMap = mutableMapOf<Pair<Int, Int>, Int>()
@@ -45,3 +21,25 @@ fun String.read(): Map<Pair<Int, Int>, Int> {
     }
     return heatMap
 }
+
+fun String.countRiskLevel(): Int {
+    val map = this.read()
+    return map.entries.sumOf {
+        val y = it.key.first
+        val x = it.key.second
+        val value = map[(y to x)]
+
+        checkNotNull(value)
+
+        val isDiagonalLower = map[(y + 1 to x + 1)] strictlyAbove value && map[(y - 1 to x - 1)] strictlyAbove value
+        val isVerticalLower = map[(y + 1 to x)] strictlyAbove value && map[(y - 1 to x)] strictlyAbove value
+        val isHorizontalLower = map[(y to x + 1)] strictlyAbove value && map[(y to x - 1)] strictlyAbove value
+        if (isDiagonalLower && isVerticalLower && isHorizontalLower) {
+            value + 1
+        } else {
+            0
+        }
+    }
+}
+
+private infix fun Int?.strictlyAbove(other: Int) = this == null || this > other
