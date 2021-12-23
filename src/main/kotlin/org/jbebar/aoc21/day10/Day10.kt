1cd3ddb639890d7f488fb8822f8c0f7323ff47ec
jbebar <jubarbe31@gmail.com>
2021-12-24 00:36:43 +0100

    clean day10

--- src/main/kotlin/org/jbebar/aoc21/day10/Day10.kt
+++ src/main/kotlin/org/jbebar/aoc21/day10/Day10.kt
@@ -57,52 +57,6 @@ private val closingCharactersToOpening = mapOf(')' to '(',
 
 class Day10B : Day {
 
-    override fun run() = countIllegalScoreText(readInputText("Day10.txt")).toLong()
-
-    fun countIllegalScoreText(text: String): Int =
-        text.lines().mapNotNull {
-            retrieveIllegalCharacter(it)
-        }.map {
-            when (it) {
-                '}' -> 1197
-                ')' -> 3
-                ']' -> 57
-                '>' -> 25137
-                else -> throw IllegalStateException()
-            }
-        }.sum()
-
-
-    fun retrieveIllegalCharacter(line: String): Char? {
-        val closingCharacterIndex = line.indexOfFirst {
-            it in closingCharactersToOpening.keys
-        }
-        if (closingCharacterIndex == -1) {
-            return null
-        }
-        val closingCharacter = line[closingCharacterIndex]
-        val analysedChunk = line.substring(0, closingCharacterIndex)
-        val openingCharacter = closingCharactersToOpening[closingCharacter]!!
-
-        if (!analysedChunk.contains(openingCharacter)) {
-            return line[closingCharacterIndex]
-        }
-
-        val openingCharacterIndex = analysedChunk.indexOfLast {
-            it == openingCharacter
-        }
-        return if (abs(openingCharacterIndex - closingCharacterIndex) == 1) {
-            val newAnalysedLine = line.filterIndexed { index, _ -> index != openingCharacterIndex && index != closingCharacterIndex }
-            retrieveIllegalCharacter(newAnalysedLine)
-        } else {
-            line[closingCharacterIndex]
-        }
-    }
-
-    private val closingCharactersToOpening = mapOf(')' to '(',
-                                                   ']' to '[',
-                                                   '}' to '{',
-                                                   '>' to '<'
-    )
+    override fun run() = TODO()
 
 }
